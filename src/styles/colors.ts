7feca28f009c6ce8834322b49a9f1634d4b36ef2
VladislavMagid <Vladislav.magid@gmail.com>
2024-12-02 14:37:02 +0100

    Корректировка сборки, удаление всех лишних элементов и подготовка старотовой сборки для дальнейшей разработки, добавление необходимых библиотек

---
+++ src/styles/colors.ts
@@ -0,0 +1,27 @@
+export interface COLORS {
+  MAIN_GREEN: string
+  PRIMARY: string
+  ERROR: string
+  PRIMARY_BLUE: string
+  PLACEHOLDER: string
+  LABEL: string
+  DISABLED: string
+  BACKGROUND_GRAY: string
+  BACKGROUND_GRAY_TRANSPARENT: string
+  GREEN_TRANSPARENT: string
+  CANCELLED: string
+}
+
+export const colors: COLORS = {
+  MAIN_GREEN: "#00BF63",
+  PRIMARY: "rgb(26, 35, 53)",
+  ERROR: "#D04648",
+  PRIMARY_BLUE: "#1f27f5",
+  PLACEHOLDER: "#6f6f6f",
+  LABEL: "#615c5c",
+  DISABLED: "#3f3f3f",
+  BACKGROUND_GRAY: "#e8e8e8",
+  BACKGROUND_GRAY_TRANSPARENT: "rgba(232, 232, 232, 0.6)",
+  GREEN_TRANSPARENT: "rgba(0, 191, 99, 0.5)",
+  CANCELLED: "gray"
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "src/assets"),
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      store: path.resolve(__dirname, "src/store"),
+      styles: path.resolve(__dirname, "src/styles"),
+    },
+  },
   plugins: [react()],
+  base: "/home",
   server: {
     open: true,
+    proxy: {
+      "/api": {
+        target: "ссылка с бэка",
+      },
+    },
   },
   test: {
     globals: true,
