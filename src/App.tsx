2589e94d2a002ea0b0becf8e8d19c6838f6c989e
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2025-03-03 22:00:44 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { SnackbarProvider } from 'notistack'
-import { DG_APP_ROUTES } from './constants/routes'
-import Layout from './components/Layout/Layout'
-import Home from './pages/Home/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import { DG_APP_ROUTES } from "./constants/routes";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return ( <SnackbarProvider maxSnack={3}>
-    <BrowserRouter>
-      <Routes>
-      <Route path={DG_APP_ROUTES.HOME} element={<Layout />}/>
-      <Route path={DG_APP_ROUTES.HOME} element={<Home/>}/>
-      
-      </Routes>
-    </BrowserRouter>
-  </SnackbarProvider>)
-    
-  
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={DG_APP_ROUTES.HOME} element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
+  );
 }
 
-export default App
+export default App;
