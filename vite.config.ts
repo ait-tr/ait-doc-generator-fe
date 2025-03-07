0a074e2742408517f2e70f8a16651d384ebe637e
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2025-03-07 19:10:57 +0100

    update viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      store: path.resolve(__dirname, 'src/store'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      constants: path.resolve(__dirname, 'src/constants'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
   },
+  
+
+ 
   test: {
     globals: true,
     environment: "jsdom",
